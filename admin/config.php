e51d6b327941455dff8fa5e78ebadd0cb4fd6812
sraban <sraban_pvt@yahoo.com>
2018-09-13 16:58:17 +0530

    deployed

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/23/admin/');
+define('HTTP_CATALOG', 'http://opencart/23/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/23/admin/');
+define('HTTPS_CATALOG', 'http://opencart/23/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/project/opencart/23/admin/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/project/opencart/23/system/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/project/opencart/23/image/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/project/opencart/23/admin/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/project/opencart/23/admin/view/template/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/project/opencart/23/system/config/');
+define('DIR_CACHE', '/opt/lampp/htdocs/project/opencart/23/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/project/opencart/23/system/storage/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/project/opencart/23/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/htdocs/project/opencart/23/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/lampp/htdocs/project/opencart/23/system/storage/upload/');
+define('DIR_CATALOG', '/opt/lampp/htdocs/project/opencart/23/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'josef');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/23/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/23/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/project/opencart/23/catalog/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/project/opencart/23/system/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/project/opencart/23/image/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/project/opencart/23/catalog/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/project/opencart/23/catalog/view/theme/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/project/opencart/23/system/config/');
+define('DIR_CACHE', '/opt/lampp/htdocs/project/opencart/23/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/project/opencart/23/system/storage/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/project/opencart/23/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/htdocs/project/opencart/23/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/lampp/htdocs/project/opencart/23/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'josef');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
